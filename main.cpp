90190de995759137ae714c28e3e9dcc03507d7e4
AlexLDybdahl <100369733+AlexLDybdahl@users.noreply.github.com>
2022-02-25 15:44:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,9 @@
-//
-// This is example code from Chapter 2.2 "The classic first program" of
-// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
-// 
-// keep_window_open() added for TDT4102, excercise 0
-
-// This program outputs the message "Hello, World!" to the monitor
-
 #include "std_lib_facilities.h"
 #include "Read_and_write.h"
 #include "count.h"
 #include "debug.h"
 #include "Catalog.h"
 #include "Temps.h"
-
-//------------------------------------------------------------------------------'
-
-// C++ programs start by executing the function main
 
 int main()
 {	
@@ -28,7 +16,6 @@ int main()
 	case 0:
 	{
 		cout << "Capitals:\n";
-
 		for (auto& elem : capitalsMap) {
 			cout << getCapital(elem.first) << endl; 
 		}
@@ -37,9 +24,7 @@ int main()
 	case 1:
 	{
 		CourseCatalog cc;
-
 		cc.testClass();
-
 		cc.addCourse("TDT4102", "c++");
 		cout << "---------" << endl;
 		cc.printCatalog();
@@ -49,11 +34,8 @@ int main()
 	case 2:
 	{
 		CourseCatalog cc;
-
 		cc.testClass();
-
 		vector<string> subjects = cc.readCatalog();
-
 		for (const auto& sub : subjects){
 			cout << sub << endl;
 		}
@@ -61,9 +43,7 @@ int main()
 	case 3:
 	{
 		Temps t;
-
 		vector<Temps> data =  t.readTemps("temperatures.txt");
-
 		t.tempStats(data);
 	}
 	default:
